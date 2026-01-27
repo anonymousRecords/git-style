f6219eaab1519cab3fd28f3bb92d0fdeeeb4ee9a
anonymousRecords <a.Records2022@gmail.com>
2026-01-27 20:10:31 +0900

    feat: api 요청 구현

---
+++ pages/api/[username]/animation.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { AnimationConfig } from "@/lib/animation/types";
+import { generatePlantAPNG } from "@/lib/theme/generator/generate-plant-apng";
+
+export const config = {
+	api: {
+		responseLimit: false,
+	},
+};
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	const { username } = req.query;
+
+	if (typeof username !== "string") {
+		res.status(400).send("Invalid username");
+		return;
+	}
+
+	// TODO : theme
+	// TODO : qualityQuery
+
+	try {
+		const apngData = await generatePlantAPNG(username, "low");
+
+		res.setHeader("Content-Type", "image/png");
+		res.setHeader("Cache-Control", "public, s-maxage=86400, max-age=3600");
+		res.setHeader("Content-Length", apngData.length);
+		res.status(200).send(Buffer.from(apngData));
+	} catch (error) {
+		console.error("[APNG GENERATION ERROR]", error);
+		res.status(500).send("Internal Server Error");
+	}
+}
