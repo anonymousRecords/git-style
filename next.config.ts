a13932b989c62baab918dcc5750c713cdbe4ba71
anonymousRecords <a.Records2022@gmail.com>
2026-01-27 20:20:26 +0900

    chore: next config를 webpack -> turbopack 설정으로 변경

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@napi-rs/canvas"],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        canvas: false,
-      };
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
