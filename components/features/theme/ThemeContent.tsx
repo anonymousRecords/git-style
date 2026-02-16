d3767ea014c29fee720d4063fb929a7de1e49dc5
anonymousRecords <a.Records2022@gmail.com>
2026-02-16 22:53:42 +0900

    chore: ThemeContent에 머리카락 테마 연결

--- components/features/theme/ThemeContent.tsx
+++ components/features/theme/ThemeContent.tsx
@@ -3,6 +3,7 @@
 import type { Theme } from "@/components/features/theme/ThemeTabs";
 import { ComingSoonContent } from "./coming-soon/ComingSoonContent";
 import { FlowerContent } from "./flower/FlowerContent";
+import { HairContent } from "./hair/HairContent";
 
 interface ThemeContentProps {
 	theme: Theme;
@@ -11,6 +12,9 @@ interface ThemeContentProps {
 export function ThemeContent({ theme }: ThemeContentProps) {
 	if (theme === "flower") {
 		return <FlowerContent />;
+	}
+	if (theme === "hair") {
+		return <HairContent />;
 	}
 	return <ComingSoonContent theme={theme} />;
 }
